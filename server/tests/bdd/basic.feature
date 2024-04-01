67ad4c7ff3a8f2d991e0fc4613cc728236fce9e3
Rudy Rodriguez <rudy_rodriguez2@apple.com>
2024-03-31 20:40:08 -0700

    Added the first scenario

---
+++ server/tests/bdd/basic.feature
@@ -0,0 +1,10 @@
+Feature: Basic
+    In order to use the app
+    As a user
+    I want to use a feature
+
+    Scenario: Basic functionality
+
+        Given The application is running
+        When I load the homepage
+        Then I see something
---
+++ server/tests/bdd/test_basic.py
@@ -0,0 +1,18 @@
+from pytest_bdd import given, scenarios, then, when
+
+scenarios("basic.feature")
+
+
+@given("The application is running")
+def the_app_is_running():
+    pass
+
+
+@when("I load the homepage")
+def i_load_the_page():
+    pass
+
+
+@then("I see something")
+def i_see_something():
+    pass

4207b5036342308ed7b300e5bcac17d6ea60dd03
Rudy Rodriguez <rudy_rodriguez2@apple.com>
2024-04-04 22:25:38 -0700

    .

--- server/tests/bdd/card/card_management.feature
+++ server/tests/bdd/card/card_management.feature
@@ -3,33 +3,33 @@ Feature: Card management
                 As a user
             I should be able to create, read, update, and delete cards
 
-  Scenario: Create cards
-    Given no cards are created
-    When creating cards
-      | num | english word | portuguese word |
-      |   1 | the          | o               |
-      |   2 | yes          | sim             |
-    Then 1 card is created
+    Scenario: Create cards
+        Given no cards are created
+        When creating cards
+            | num | english word | portuguese word |
+            | 1   | the          | o               |
+            | 2   | yes          | sim             |
+        Then 2 cards are created
 
-  Scenario: Edit card
-    Given the following existing cards
-      | num | english word | portuguese word |
-      |   1 | the          | o               |
-      |   2 | car          | carro           |
-    When updating card 2 with values
-      | english word | portuguese word |
-      | cat          | gato            |
-    Then the following cards exist
-      | num | english word | portuguese word |
-      |   1 | the          | o               |
-      |   2 | cat          | gato            |
+    Scenario: Edit card
+        Given the following existing cards
+            | num | english word | portuguese word |
+            | 1   | the          | o               |
+            | 2   | car          | carro           |
+        When updating card 2 with values
+            | english word | portuguese word |
+            | cat          | gato            |
+        Then the following cards exist
+            | num | english word | portuguese word |
+            | 1   | the          | o               |
+            | 2   | cat          | gato            |
 
-  Scenario: Delete card
-    Given the following existing cards
-      | num | english word | portuguese word |
-      |   1 | the          | o               |
-      |   2 | car          | o               |
-    When deleting card 1
-    Then the following cards exist
-      | num | english word | portuguese word |
-      |   2 | cat          | o               |
+    Scenario: Delete card
+        Given the following existing cards
+            | num | english word | portuguese word |
+            | 1   | the          | o               |
+            | 2   | car          | carro           |
+        When deleting card 1
+        Then the following cards exist
+            | num | english word | portuguese word |
+            | 2   | cat          | o               |
--- server/tests/bdd/card/test_card_management.py
+++ server/tests/bdd/card/test_card_management.py
@@ -1,22 +1,24 @@
 from pytest_bdd import given, parsers, scenarios, then, when
-from table_parser.table_parser import table_parser
+from table_parser.table_parser import table_parser  # type: ignore
+
+from models.models import Cards
 
 scenarios("card_management.feature")
 
 
-def fix_table(data):
+def fix(data):
     return [dict(zip(data.keys(), values)) for values in zip(*data.values())]
 
 
 @given("no cards are created")
 def no_cards_are_created(clear):
+    # called clear already here
     pass
 
 
 @when(parsers.cfparse("creating cards\n{table}"))
 def creating_cards(table, cards_factory, session):
-    table_dict = table_parser(table)
-    table = fix_table(table_dict)
+    table = fix(table_parser(table))
     for row in table:
         card = cards_factory.create(
             number=row["num"],
@@ -27,24 +29,43 @@ def creating_cards(table, cards_factory, session):
         session.commit()
 
 
-@then(parsers.parse("{:d} card is created"))
-def card_is_created():
-    pass
+@then(parsers.parse("{num_cards:d} cards are created"))
+def card_is_created(num_cards, session):
+    num_rows = session.query(Cards).count()
+    assert num_rows == num_cards
 
 
 @given(parsers.cfparse("the following existing cards\n{table}"))
-def the_following_existing_cards(table):
-    pass
+def the_following_existing_cards(clear, table, cards_factory, session):
+    table = fix(table_parser(table))
+    for row in table:
+        card = cards_factory.create(
+            number=row["num"],
+            english_word=row["english word"],
+            portuguese_word=row["portuguese word"],
+        )
+        session.add(card)
+        session.commit()
 
 
 @when(parsers.cfparse("updating card {card_num} with values\n{table}"))
-def updating_card_with_values():
-    pass
+def updating_card_with_values(card_num, table, session):
+    card = session.query(Cards).where(Cards.number == card_num).first()
+    table = table_parser(table)
+    card.english_word = table["english word"][0]
+    card.portuguese_word = table["portuguese word"][0]
+    session.add(card)
+    session.commit()
 
 
 @then(parsers.cfparse("the following cards exist\n{table}"))
-def the_following_cards_exist():
-    pass
+def the_following_cards_exist(table, session):
+    table = fix(table_parser(table))
+    cards = session.query(Cards).all()
+    for row, card in zip(table, cards):
+        assert int(row["num"]) == card.number
+        assert row["english word"] == card.english_word
+        assert row["portuguese word"] == card.portuguese_word
 
 
 @when(parsers.parse("deleting card {card_num}"))
